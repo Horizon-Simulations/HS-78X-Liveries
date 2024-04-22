ab2ca78907c7c599f570a751cbcf263a731c9cb7
markszutor <szutor.mark@gmail.com>
2024-04-22 22:11:27 +0300

    Update fragment-hs78X-liveries.js

--- scripts/fragment-hs78X-liveries.js
+++ scripts/fragment-hs78X-liveries.js
@@ -8,122 +8,20 @@ const execute = async () => {
             baseDir: './hs-78X-liveries/out/horizonsim-aircraft-787-10-livery-package',
             outDir: './hs-78X-liveries/out/build-modules',
             modules: [{
-                name: 'Air_Canada',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ACA'
-            }, {
-                name: 'Air_Canada_2',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ACA2'
-            }, {
-                name: 'Air_Canada_3',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ACA3'
-            }, {
-                name: 'Air_Europa',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_AEA'
-            }, {
-                name: 'Air_France',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_AFR'
-            }, {
-                name: 'All_Nippon_Airways',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ANA'
-            }, {
-                name: 'All_Nippon_Airways_Star_Wars',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ANA2'
-            }, {
-                name: 'Air_New_Zealand',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ANZ'
-            }, {
-                name: 'Air_New_Zealand_2',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ANZ2'
+                name: 'Horizon_Simulations_House_Livery',
+                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_HR_SIM'
             }, {
                 name: 'British_Airways',
                 sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_BAW'
             }, {
-                name: 'TUI_fly_Nordic',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_BLX'
-            }, {
-                name: 'Air_China',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CCA'
-            }, {
-                name: 'China_Eastern_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CES'
-            }, {
-                name: 'Hainan_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CHH'
-            }, {
-                name: 'Shanghai_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CSH'
-            }, {
-                name: 'China_Southern_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CSN'
-            }, {
-                name: 'Xiamen_Air',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CXA'
-            }, {
-                name: 'Xiamen_Air_2',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_CXA2'
-            }, {
-                name: 'Juneyao_Air',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_DKH'
-            }, {
-                name: 'Lufthansa',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_DLH'
-            }, {
-                name: 'El_Al',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ELY'
-            }, {
-                name: 'El_Al_2',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ELY2'
-            }, {
-                name: 'Etihad',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_ETD'
-            }, {
-                name: 'Japan_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_JAL'
-            }, {
-                name: 'Korean_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_KAL'
-            }, {
-                name: 'KLM_Royal_Dutch_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_KLM'
-            }, {
-                name: 'LOT_Polish_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_LOT'
-            }, {
-                name: 'Egyptair',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_MSR'
-            }, {
-                name: 'Norse_Atlantic_Airways',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_NBT'
-            }, {
-                name: 'Qantas',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_QFA'
-            }, {
-                name: 'Saudia',
+                name: 'Saudia_1',
                 sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_SVA'
             }, {
-                name: 'Scoot',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_TGW'
+                name: 'Saudia_2',
+                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_SVA2'
             }, {
-                name: 'Thai_Airways_International',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_THA'
-            }, {
-                name: 'TUI_Airways',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_TOM'
-            }, {
-                name: 'United_Airlines',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_UAL'
-            }, {
-                name: 'Norse_Atlantic_UK',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_UBT'
-            }, {
-                name: 'Virgin_Atlantic',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_VIR'
-            }, {
-                name: 'Vistara',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_VTI'
-            }, {
-                name: 'Westjet',
-                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_WJA'
+                name: 'Vietnam_Airlines',
+                sourceDir: './SimObjects/Airplanes/HorizonSim_B787_10_HVN'
             }]
         });
         console.log(result);
